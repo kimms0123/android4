1afce9f8caae3f47d444465e188c58c17d1567d8
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2025-07-07 14:10:54 +0900

    day 24

--- app/src/main/java/com/example/android2/exam22.kt
+++ app/src/main/java/com/example/android2/exam22.kt
@@ -13,10 +13,15 @@ import androidx.core.net.toUri
     액티비티와 인텐트의 응용
     2. 암시적 인텐트
         - 약속된 액션을 지정하여 안드로이드에서 제공하는 기존 응용 프로그램을 실행
+        👉 안드로이드 시스템에 "이런 행동을 하고 싶다"고 알려서 해당 액션을 처리할 수 있는 앱을 찾아 실행하는 방법
         [암시적 인텐트 개념 그림]
-    [전화 걸기/구글맵 사용 권한 추가]
+        [대표적인 암시적 인텐트 종류]
+    [전화 걸기/구글맵(필수) 사용 권한 추가]
     ```
     <uses-permission android:name="android.permission.CALL_PHONE"/>
+    <!-- ⇓ 해당 디바이스가 전화 기능(telephony hardware)을 요구하는지 여부를 명시해야 함.(안하면 위에 코드 오류남) -->
+    <uses-feature android:name="android.hardware.telephony" android:required="false" />
+
     <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
     <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
     ```
@@ -34,18 +39,21 @@ class exam22 : AppCompatActivity() {
         var btnSms = findViewById<Button>(R.id.btnSms)
         var btnPhoto = findViewById<Button>(R.id.btnPhoto)
 
+        // 전화 앱 열기
         btnDial.setOnClickListener {
             var uri = Uri.parse("tel:010-1234-5678") // 전화 걸기 위해 URI 문자열을 tel:전화번호 형식으로 사용
-            var intent = Intent(Intent.ACTION_DIAL, uri) // ACTION_DIAL:
+            var intent = Intent(Intent.ACTION_DIAL, uri) // ACTION_DIAL: 전화번호를 다이얼화면에 표시하고 사용자 확인 후 통화 가능
             startActivity(intent)
         }
 
+        // 웹 브라우저 열기
         btnWeb.setOnClickListener {
             var uri = Uri.parse("https://velog.io/") // 웹 브라우저 열기 위한 URI "http://~~" 문자열 형식
-            var intent = Intent(Intent.ACTION_VIEW, uri) // ACTION_VIEW:
+            var intent = Intent(Intent.ACTION_VIEW, uri) // ACTION_VIEW: URL, 이미지, 파일 등 콘텐츠를 적절한 앱으로 보여줌
             startActivity(intent)
         }
 
+        // 구글맵 열기
         btnGoogle.setOnClickListener {
             // 구글맵 주소와 경위도 형식 사용
             var uri = Uri.parse("https://www.google.co.kr/maps/place/%EC%9D%B8%EC%B2%9C%EA%B4%91%EC%97%AD%EC%8B%9C/@35.737686,123.0633915,7z/data=!3m1!4b1!4m6!3m5!1s0x35796f2596138247:0x7d37fd902cb76142!8m2!3d37.4751578!4d126.6312666!16zL20vMGh2MWI?hl=ko&entry=ttu&g_ep=EgoyMDI1MDYzMC4wIKXMDSoASAFQAw%3D%3D")
@@ -53,23 +61,29 @@ class exam22 : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // 웹 검색 실행
         btnSearch.setOnClickListener {
-            var intent = Intent(Intent.ACTION_WEB_SEARCH) // ACTION_WEB_SEARCH:
+            var intent = Intent(Intent.ACTION_WEB_SEARCH) // ACTION_WEB_SEARCH: 웹 검색(구글, 네이버 등) 실행하여 검색어로 검색
             // 검색을 위해 putExtra()로 넘기는데, 첫 파라미터로 SearchManager.QUERY를 사용하고
             // 두번째 파라미터에는 검색할 단어를 넣음
             intent.putExtra(SearchManager.QUERY, "안드로이드")
             startActivity(intent)
         }
 
+        // 문자 메시지 앱 열기
         btnSms.setOnClickListener {
-            var intent = Intent(Intent.ACTION_SENDTO) // ACTION_SENDTO:
+            var intent = Intent(Intent.ACTION_SENDTO) // ACTION_SENDTO: 문자나 메일같은 특정 URI 스킴으로만 동작하는 앱 실행
             intent.putExtra("sms_body", "안녕하세요") // putExtra()로 첫 파라미터에 "sms_body"를 넣고, 두번째에 보낼 문자를 넣어 넘김
-            intent.data = Uri.parse("smsto:" + Uri.encode("010-1234-5678")) // intent.data 속성에 값을 설정해야한다(이유 설명 필요)
+            // intent.data 속성에 값을 설정 필수 -> 어떤 앱으로 보낼지 지정하는 역할
+            intent.data = Uri.parse("smsto:" + Uri.encode("010-1234-5678"))
             startActivity(intent)
         }
 
+        // 카메라앱 실행
+        // 실행 결과 가상화면이 나옴 => Emulator에는 실제 카메라 센서가 없어서 기본제공 가상 카메라화면이 나옴
+        // 진짜 웹캠 영상 테스트 -> AVD Manager → 해당 가상 디바이스 Edit → Camera 설정에서 Front / Back 카메라를 Emulated 대신 Webcam0 변경
         btnPhoto.setOnClickListener {
-            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) // ACTION_IMAGE_CAPTURE:
+            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE) // ACTION_IMAGE_CAPTURE: 카메라 앱을 실행하여 사진 촬영 화면 표시
             startActivity(intent)
         }
     }
